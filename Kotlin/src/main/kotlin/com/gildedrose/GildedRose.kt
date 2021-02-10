16a646985d2f8067f1dbcda8aff569d18e064f36
Vegim Hasani <vegimhasani@gmail.com>
2021-02-10 17:42:24 +0100

    Start of the refactoring, extracting the methods

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,51 +3,60 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+        items.indices.forEach {
+            val item = items[it]
+            updateSingleItem(item)
+        }
+    }
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+    private fun incrementQuality(item: Item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+        }
+    }
+
+    private fun updateSingleItem(item: Item) {
+        updateQuality(item)
+
+        if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.sellIn = item.sellIn - 1
+        }
+
+        if (item.sellIn < 0) {
+            if (item.name != "Aged Brie") {
+                if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.quality > 0) {
+                        if (item.name != "Sulfuras, Hand of Ragnaros") {
+                            item.quality = item.quality - 1
                         }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    item.quality = item.quality - item.quality
+                }
+            } else {
+                incrementQuality(item)
+            }
+        }
+    }
+
+    private fun updateQuality(item: Item) {
+        if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.quality > 0) {
+                if (item.name != "Sulfuras, Hand of Ragnaros") {
+                    item.quality = item.quality - 1
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.sellIn < 11) {
+                        incrementQuality(item)
+                    }
+
+                    if (item.sellIn < 6) {
+                        incrementQuality(item)
                     }
                 }
             }
