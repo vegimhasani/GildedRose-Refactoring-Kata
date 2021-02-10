1b9a2ed6ea5944c6835d323353277e8690a2eb40
Vegim Hasani <vegimhasani@gmail.com>
2021-02-10 17:40:49 +0100

    Fix unit test

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -7,10 +7,10 @@ internal class GildedRoseTest {
 
     @Test
     fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+        val items = arrayOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
 
     }
 
