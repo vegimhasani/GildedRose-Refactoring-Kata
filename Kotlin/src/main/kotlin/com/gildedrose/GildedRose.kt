e929dac05dd35907c6a7880a40bd25e5a98b6678
Vegim Hasani <vegimhasani@gmail.com>
2021-02-10 21:43:23 +0100

    Use the strategy implementation

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,83 +1,18 @@
 package com.gildedrose
 
+import com.gildedrose.factory.ItemStrategyFactoryImpl
+import com.gildedrose.strategy.ItemStrategy
+
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         items.indices.forEach {
             val item = items[it]
-            updateSingleItem(item)
+            val strategy = handleStrategy(item)
+            strategy.init(item)
         }
     }
 
-    private fun incrementQuality(item: Item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1
-        }
-    }
-
-    private fun updateSingleItem(item: Item) {
-        updateQuality(item)
-
-        decrementSellin(item)
-
-        if (isSellinExpired(item)) {
-            updateSellinExpired(item)
-        }
-    }
-
-    private fun isSellinExpired(item: Item) = item.sellIn < 0
-
-    private fun updateSellinExpired(item: Item) {
-        when (item.name) {
-            "Aged Brie" -> {
-                incrementQuality(item)
-            }
-            "Backstage passes to a TAFKAL80ETC concert" -> {
-                item.quality = item.quality - item.quality
-            }
-            "Sulfuras, Hand of Ragnaros" -> {
-                return
-            }
-            else -> {
-                decrementQuality(item)
-            }
-        }
-    }
-
-    private fun decrementQuality(item: Item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1
-        }
-    }
-
-    private fun decrementSellin(item: Item) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.sellIn = item.sellIn - 1
-        }
-    }
-
-    private fun updateQuality(item: Item) {
-        when (item.name) {
-            "Aged Brie" -> {
-                incrementQuality(item)
-            }
-            "Backstage passes to a TAFKAL80ETC concert" -> {
-                incrementQuality(item)
-                updateBackstage(item)
-            }
-            "Sulfuras, Hand of Ragnaros" -> return
-            else -> decrementQuality(item)
-        }
-    }
-
-    private fun updateBackstage(item: Item) {
-        if (item.sellIn < 11) {
-            incrementQuality(item)
-        }
-
-        if (item.sellIn < 6) {
-            incrementQuality(item)
-        }
-    }
-
+    private fun handleStrategy(item: Item) : ItemStrategy =
+        ItemStrategyFactoryImpl().createItemStrategyFromName(item.name)
 }
